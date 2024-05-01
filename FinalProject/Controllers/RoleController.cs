b8b87165b4877e518979cb3147e8930292f4d7ac
yassmine <yassminemohamad2001@gmail.com>
2024-05-02 01:45:51 +0300

    RoleController

---
+++ FinalProject/Controllers/RoleController.cs
@@ -0,0 +1,48 @@
+﻿using FinalProject.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class RoleController : Controller
+    {
+        
+            RoleManager<IdentityRole> roleManager;
+
+            public RoleController(RoleManager<IdentityRole> roleManager)
+            {
+                this.roleManager = roleManager;
+            }
+
+            [HttpGet]
+            public IActionResult Create()
+            {
+                return View();
+
+            }
+
+            [HttpPost]
+            
+            public async Task<IActionResult> Create(CreateRoleViewModel roleVm)
+            {
+                if (ModelState.IsValid)
+                {
+                    IdentityRole role = new IdentityRole(roleVm.Name);
+
+                    var result = await roleManager.CreateAsync(role);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid Role.");
+                    }
+                }
+                return View();
+
+            }
+        }
+}
---
+++ FinalProject/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
