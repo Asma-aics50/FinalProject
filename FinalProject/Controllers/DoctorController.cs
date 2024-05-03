ead1ea911ec5fff67bcb62fe11010c0861ce950d
yassmine <yassminemohamad2001@gmail.com>
2024-05-04 02:44:31 +0300

    View AdminDasboard using(Doctor/index)

---
+++ FinalProject/Controllers/DoctorController.cs
@@ -0,0 +1,26 @@
+﻿using FinalProject.IRepositry;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class DoctorController : Controller
+    {
+        
+        IDoctorRepositry doctorRepositry;
+        
+        public DoctorController(
+           
+            IDoctorRepositry doctorRepositry
+           )
+        {
+           
+            this.doctorRepositry = doctorRepositry;
+           }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ FinalProject/ViewModels/DoctorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.ViewModels
+{
+    public class DoctorViewModel 
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateOnly BirthDate { get; set; }
+    }
+}
