bca92e8f9d7830922c44e17bcb8bdc16c2b8f53f
yassmine <yassminemohamad2001@gmail.com>
2024-05-04 23:35:40 +0300

    Admin-Dashboard index

---
+++ FinalProject/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using FinalProject.Data;
+using FinalProject.IRepositry;
+using FinalProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        IPatientRepositry patientRepositry;
+           IDoctorRepositry doctorRepositry;
+              IEmployeeRepositry employeeRepositry;
+        public AdminController(
+              IPatientRepositry patientRepositry,
+              IDoctorRepositry doctorRepositry,
+              IEmployeeRepositry employeeRepositry
+           )
+        {
+            this.patientRepositry = patientRepositry;
+            this.doctorRepositry = doctorRepositry;
+            this.employeeRepositry = employeeRepositry;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["DoctorsNum"]=doctorRepositry.GetAll().ToList().Count();
+            ViewData["PatientsNum"]=patientRepositry.GetAll().ToList().Count();
+
+
+
+           
+            return View();
+        }
+
+
+
+    }
+}
