2f5a53400d8bcf9d43c17148a4a43b3ae78e69dd
yassmine <yassminemohamad2001@gmail.com>
2024-05-22 11:13:29 +0300

    AdminIndex- add doctor button

--- FinalProject/Controllers/DashboardController.cs
+++ FinalProject/Controllers/DashboardController.cs
@@ -9,25 +9,40 @@ namespace FinalProject.Controllers
             IPatientRepositry patientRepositry;
             IDoctorRepositry doctorRepositry;
         IBookedAppointmentsRepositry bookedAppointmentsRepositry;
-
-
-
+        IEmployeeRepositry employeeRepositry;
+        IPatientHistoryRepositry PatientHistoryRepositry;
+        IBillRepositry BillRepositry;
             public DashboardController(
                   IPatientRepositry patientRepositry,
                   IDoctorRepositry doctorRepositry,
-                  IBookedAppointmentsRepositry bookedAppointmentsRepositry
+                  IBookedAppointmentsRepositry bookedAppointmentsRepositry,
+                  IEmployeeRepositry employeeRepositry,
+                  IPatientHistoryRepositry patientHistoryRepositry,
+                  IBillRepositry BillRepositry
+
                )
             {
                 this.patientRepositry = patientRepositry;
                 this.doctorRepositry = doctorRepositry;
                 this.bookedAppointmentsRepositry = bookedAppointmentsRepositry;
+                this.employeeRepositry = employeeRepositry;
+                this.PatientHistoryRepositry = patientHistoryRepositry;
+            this.BillRepositry = BillRepositry;
             }
 
             public IActionResult AdminIndex()
             {
                 ViewData["DoctorsNum"] = doctorRepositry.GetAll().ToList().Count();
                 ViewData["PatientsNum"] = patientRepositry.GetAll().ToList().Count();
-                return View();
+                ViewData["AppointmnetsNum"] = bookedAppointmentsRepositry.GetAll().ToList().Count();
+                ViewData["employeesNum"] = employeeRepositry.GetAll().ToList().Count();
+                ViewData["CaseStudies"]=PatientHistoryRepositry.GetAll().ToList().Count();
+                 ViewData["Invoices"]=PatientHistoryRepositry.GetAll().ToList().Count();
+                
+
+
+
+            return View();
 
             }
             public IActionResult DoctorIndex()
